b893129be76c6d491b596fc09a008fcdfc1e53ff
Aaron Parecki <aaron@parecki.com>
2015-09-02 19:42:45 -0700

    reject non-root domain logins
    in order to support this, there needs to be a way to convert IndieAuth URLs with a path component into something URL safe

--- controllers/auth.rb
+++ controllers/auth.rb
@@ -7,24 +7,32 @@ class Controller < Sinatra::Base
   get '/auth/indieauth/callback' do
     auth = request.env["omniauth.auth"]
     puts auth.info.url.inspect
-    domain = URI.parse(auth.info.url).host.downcase
 
-    user = Account.first :domain => domain
+    signed_in_uri = URI.parse(auth.info.url)
 
-    if user.nil?
-      user = Account.new
-      user.username = domain
-      user.domain = domain
-      user.created_at = Time.now
-      user.updated_at = Time.now
+    if !['','/'].include? signed_in_uri.path
+      @message = "Sorry, you can't use this service if your IndieAuth URL contains a path component. Only root domains are supported.<br><br>You signed in as <code>#{auth.info.url}</code>"
+      erb :error
+    else
+      domain = signed_in_uri.host.downcase
+
+      user = Account.first :domain => domain
+
+      if user.nil?
+        user = Account.new
+        user.username = domain
+        user.domain = domain
+        user.created_at = Time.now
+        user.updated_at = Time.now
+      end
+
+      user.last_login = Time.now
+      user.save
+
+      session[:user_id] = user[:id]
+      puts "User successfully logged in"
+      redirect "/dashboard"
     end
-
-    user.last_login = Time.now
-    user.save
-
-    session[:user_id] = user[:id]
-    puts "User successfully logged in"
-    redirect "/dashboard"
   end
 
 end
