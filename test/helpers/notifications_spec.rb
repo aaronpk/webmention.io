70fef420a158d04ecb09460dfcc3916040d190b5
Aaron Parecki <aaron@parecki.com>
2015-11-09 12:27:47 -0500

    add test for one source -> two targets

--- test/helpers/notifications_spec.rb
+++ test/helpers/notifications_spec.rb
@@ -5,8 +5,6 @@ describe NotificationQueue do
   before do
     TestData.stub_requests self
     @w = WebmentionProcessor.new
-    # Reset the DB between tests
-    DataMapper.auto_migrate!
   end
 
   describe "one source linked to many targets" do
@@ -30,6 +28,27 @@ describe NotificationQueue do
 
       notifications.length.must_equal 1
       notifications[0].text.must_equal "Source Author wrote a post that linked to http://example.com/target/entry"
+    end
+
+    it "works with one source linking to two targets" do
+      @site = Site.create :domain => "example.com", :account => @account
+      @page1 = Page.create :href => "http://example.com/target/entry", :site => @site, :account => @account
+      @page2 = Page.create :href => "http://example.com/target/photo", :site => @site, :account => @account
+
+      source = "http://source.example.org/notification/one-to-two"
+      entry = @w.get_entry_from_source source
+
+      link1 = Link.create :page => @page1, :href => source, :site => @site
+      link2 = Link.create :page => @page2, :href => source, :site => @site
+      @w.add_author_to_link entry, link1
+      @w.add_author_to_link entry, link2
+      phrase = @w.get_phrase_and_set_type entry, link1, source, @page1
+      phrase = @w.get_phrase_and_set_type entry, link2, source, @page2
+
+      notifications = NotificationQueue.generate_notifications @site, [link1.id, link2.id], [link1.id, link2.id]
+
+      notifications.length.must_equal 1
+      notifications[0].text.must_equal "Source Author wrote a post that linked to http://example.com/target/entry and http://example.com/target/photo"
     end
 
   end
--- test/load.rb
+++ test/load.rb
@@ -2,3 +2,6 @@ require_relative '../environment'
 require_relative './testdata'
 require 'minitest/autorun'
 require 'webmock/minitest'
+
+# Reset the DB between test runs
+DataMapper.auto_migrate!
