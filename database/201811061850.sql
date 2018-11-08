1ce46030201ef05ad72cb343576e3730f5ab9926
Aaron Parecki <aaron@parecki.com>
2018-11-08 11:06:09 -0800

    fix debug migration

--- database/201811061850.sql
+++ database/201811061850.sql
@@ -1,6 +1,6 @@
-CREATE TABLE `debug` (
+CREATE TABLE `debugs` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `page_url` varchar(255) DEFAULT NULL,
-  `debug` tinyint(4) NOT NULL DEFAULT '0',
+  `enabled` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
